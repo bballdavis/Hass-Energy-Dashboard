ef70c7dd889f9032ebef970630576e4dcd26ae86
bballdavis <bballdavis@hotmail.com>
2025-03-25 19:53:57 -0500

    a new-new approach

--- energy-dashboard-cards.js
+++
@@ -1,5 +0,0 @@
-// This file is deprecated.
-// Do not include this file in your Lovelace configuration;
-// HACS will install the two individual card files:
-// • energy-dashboard-entity-card.js
-// • energy-dashboard-chart-card.js
